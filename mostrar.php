8f8fb2a72ca6de8bd898d4099367571773b242f6
fepe15 <fedegperalta@gmail.com>
2016-03-28 21:56:55 -0300

    Pruebas de direccionamiento

--- mostrar.php
+++ mostrar.php
@@ -1,22 +1,27 @@
-<h1>Datos recibidos</h1>
 <?php
+if (isset($_POST["boton1"])) {
 
 
+
+echo "<h1>". "Datos recibidos" . "</h1>";
 echo "recibidos: <br>";
 // var_dump($_POST); ( no muestra el envio de datos por la barra de direcciones) (NO SE PUEDE GUARDAR EN FAVORITOS, NO SE PUEDE IR HACIA ATRAS EN BACK HISTORY, no tiene limite de envio)
 // var_dump($_GET); (puedo ver la info que envio, puedo guardar en favoritos, puedo ir hacia atras en back history, puedo guardar en cache, limite de 2048 caracteres)
-
+var_dump($_POST);
 
 $user=$_POST["usuario"];
 //echo $user . "<br>";
 $password=$_POST["clave"];
 //echo $password;
 
-if ($user=="admin" && $password=="1234") {
-//	echo "Bievenido";
-	include_once "bienvenido.html";
-	}
-	else
-//		echo "usted no esta logueado";
- 	include_once "nologueado.html";
+		if ($user=="admin" && $password=="1234") {
+	//	echo "Bievenido";
+		include_once "bienvenido.html";
+		}
+		else
+	//		echo "usted no esta logueado";
+	 	include_once "nologueado.html";
+}
+
+
 ?>
---
+++ testeo.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>RODRI BIGOTON</title>
+</head>
+<body>
+
+	<h1>RODRI GATO</h1>
+
+</body>
+</html>
